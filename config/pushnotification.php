1c7c9f598974c8bd35a40a619910200d6fd04375
sa3d01 <sa3dsalem01@gmail.com>
2021-04-10 06:38:06 +0200

    order room

--- config/pushnotification.php
+++ config/pushnotification.php
@@ -12,7 +12,7 @@ return [
     'fcm' => [
         'priority' => 'normal',
         'dry_run' => false,
-        'apiKey' => 'AAAAbynyft0:APA91bHGJkmxRIqPsv1skBapw4-tKe30Ex4FTyD8A4oB8bhNNdYTsUXKGgLfD1pmFFMeukpksuQWGtAFFQeqUOYl_IB_-u2JDoPx-lyaDpYfwJchMWnx1czSEYRLe5x0_WyJOFEQh9Wa',
+        'apiKey' => 'AAAACKXdzhU:APA91bFzmNuYiQaoLn5zZqqJoAi3aVihcrIyEYqN2CJfUEKB89MDXXCVZ8j5o8wyNJQeaex4pJklzQfLIySBYJB2WIzqCzq42pnsN3ocsFxGkHP_-KaqT_SRhKIo0Ll3px4s8hD6LJzx',
     ],
     'apn' => [
         'certificate' => __DIR__ . '/iosCertificates/apns-dev-cert.pem',
