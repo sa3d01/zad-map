cab935b4d74a342af79c138404f64febe18cf134
sa3d01 <sa3dsalem01@gmail.com>
2021-08-26 00:21:35 +0200

    new fucking edits

--- app/Http/Resources/DeliveryLoginResourse.php
+++ app/Http/Resources/DeliveryLoginResourse.php
@@ -3,6 +3,8 @@
 namespace App\Http\Resources;
 
 use App\Models\Car;
+use App\Models\Delivery;
+use App\Models\Provider;
 use App\Models\User;
 use Carbon\Carbon;
 use Illuminate\Http\Resources\Json\JsonResource;
@@ -19,6 +21,7 @@ class DeliveryLoginResourse extends JsonResource
 
     public function toArray($request)
     {
+        $delivery=Delivery::where('user_id',$this->id)->first();
         $token = auth('api')->login(User::find($this->id));
         $car = Car::where('user_id', $this->id)->latest()->first();
         if (!$car) {
@@ -27,9 +30,9 @@ class DeliveryLoginResourse extends JsonResource
             $car_model = new CarResourse($car);
         }
         $devices=[];
-        $devices[]=$this->delivery->devices;
+        $devices[]=$delivery->devices;
         $devices[]=$request['device.id'];
-        $this->delivery->update([
+        $delivery->update([
             'devices' => $devices,
             'last_login_at' => Carbon::now(),
             'last_ip' => $request->ip(),
@@ -38,30 +41,30 @@ class DeliveryLoginResourse extends JsonResource
         return [
             "user" => [
                 'id' => (int)$this->id,
-                'online' => $this->delivery->online,
+                'online' => $delivery->online,
                 'rating' => (double)$this->averageRate(),
                 'feedBacks' => $this->feedbacks(),
                 'type' => $request['type'],
-                'name' => $this->delivery->name,
+                'name' => $delivery->name,
                 'phone' => $this->phone ?? "",
                 'city' => [
-                    'id' => $this->delivery->city ? (int)$this->delivery->city->id : 0,
-                    'name' => $this->delivery->city ? $this->delivery->city->name : "",
+                    'id' => $delivery->city ? (int)$delivery->city->id : 0,
+                    'name' => $delivery->city ? $delivery->city->name : "",
                 ],
                 'district' => [
-                    'id' => $this->delivery->district ? (int)$this->delivery->district->id : 0,
-                    'name' => $this->delivery->district ? $this->delivery->district->name : "",
+                    'id' => $delivery->district ? (int)$delivery->district->id : 0,
+                    'name' => $delivery->district ? $delivery->district->name : "",
                 ],
-                'location'=>$this->delivery->location,
-                'image' => $this->delivery->image,
+                'location'=>$delivery->location,
+                'image' => $delivery->image,
                 'has_delivery'=> 0,
                 'delivery_price'=> 0,
                 'car' => $car_model,
-                'banks' => new BankCollection($this->delivery->banks),
+                'banks' => new BankCollection($delivery->banks),
             ],
             "settings" => [
-                'approved' => $this->delivery->approved,
-                'banned' => $this->delivery->banned,
+                'approved' => $delivery->approved,
+                'banned' => $delivery->banned,
             ],
             "access_token" => [
                 'token' => $token,
--- app/Http/Resources/ProviderLoginResourse.php
+++ app/Http/Resources/ProviderLoginResourse.php
@@ -3,6 +3,8 @@
 namespace App\Http\Resources;
 
 use App\Models\Car;
+use App\Models\NormalUser;
+use App\Models\Provider;
 use App\Models\User;
 use Carbon\Carbon;
 use Illuminate\Http\Resources\Json\JsonResource;
@@ -17,44 +19,14 @@ class ProviderLoginResourse extends JsonResource
      * @return array
      */
 
-    private function userData()
-    {
-        $devices[]=$this->normal_user->devices;
-        $devices[]=request()->input('device.id');
-        $this->normal_user->update([
-            'devices' => $devices,
-            'last_login_at' => Carbon::now(),
-            'last_ip' => request()->ip(),
-        ]);
-        $arr['name'] = $this->normal_user->name;
-        $arr['online'] = false;
-        $arr['rating'] = 0;
-        $arr['feedBacks'] = [];
-        $arr['type'] = request()->input('type');
-        $arr['phone'] =$this->phone;
-
-        $arr['city'] = [
-            'id' => $this->normal_user->city ? (int)$this->normal_user->city->id : 0,
-            'name' => $this->normal_user->city ? $this->normal_user->city->name : "",
-        ];
-        $arr['district'] = [
-            'id' => $this->normal_user->district ? (int)$this->normal_user->district->id : 0,
-            'name' => $this->normal_user->district ? $this->normal_user->district->name : "",
-        ];
-        $arr['location'] =$this->normal_user->location;
-        $arr['image'] =$this->normal_user->image;
-        $arr['has_delivery'] =0;
-        $arr['delivery_price'] =0;
-        $arr['banks'] =[];
-        return $arr;
-    }
     public function toArray($request)
     {
+        $provider=Provider::where('user_id',$this->id)->first();
         $token = auth('api')->login(User::find($this->id));
         $devices=[];
-        $devices[]=$this->provider->devices;
+        $devices[]=$provider->devices;
         $devices[]=$request['device.id'];
-        $this->provider->update([
+        $provider->update([
             'devices' => $devices,
             'last_login_at' => Carbon::now(),
             'last_ip' => $request->ip(),
@@ -62,30 +34,30 @@ class ProviderLoginResourse extends JsonResource
         return [
             "user" => [
                 'id' => (int)$this->id,
-                'online' => $this->provider->online,
+                'online' => $provider->online,
                 'rating' => (double)$this->averageRate(),
                 'feedBacks' => $this->feedbacks(),
                 'type' => $request['type'],
-                'name' => $this->provider->name,
+                'name' => $provider->name,
                 'phone' => $this->phone ?? "",
                 'city' => [
-                    'id' => $this->provider->city ? (int)$this->provider->city->id : 0,
-                    'name' => $this->provider->city ? $this->provider->city->name : "",
+                    'id' => $provider->city ? (int)$provider->city->id : 0,
+                    'name' => $provider->city ? $provider->city->name : "",
                 ],
                 'district' => [
-                    'id' => $this->provider->district ? (int)$this->provider->district->id : 0,
-                    'name' => $this->provider->district ? $this->provider->district->name : "",
+                    'id' => $provider->district ? (int)$provider->district->id : 0,
+                    'name' => $provider->district ? $provider->district->name : "",
                 ],
-                'location'=>$this->provider->location,
-                'image' => $this->provider->image,
-                'has_delivery'=> (int)$this->provider->has_delivery,
-                'delivery_price'=> (int)$this->provider->delivery_price,
+                'location'=>$provider->location,
+                'image' => $provider->image,
+                'has_delivery'=> (int)$provider->has_delivery,
+                'delivery_price'=> (int)$provider->delivery_price,
                 'car' => new Object_(),
-                'banks' => new BankCollection($this->provider->banks),
+                'banks' => new BankCollection($provider->banks),
             ],
             "settings" => [
-                'approved' => $this->provider->approved,
-                'banned' => $this->provider->banned,
+                'approved' => $provider->approved,
+                'banned' => $provider->banned,
             ],
             "access_token" => [
                 'token' => $token,
