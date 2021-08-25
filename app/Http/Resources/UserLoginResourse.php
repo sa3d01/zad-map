ec4c8017dcc79a29785637743c8ac7cf6e2b9e19
sa3d01 <sa3dsalem01@gmail.com>
2021-08-26 00:16:26 +0200

    new fucking edits

--- app/Http/Resources/UserLoginResourse.php
+++ app/Http/Resources/UserLoginResourse.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Resources;
 
+use App\Models\NormalUser;
 use App\Models\User;
 use Carbon\Carbon;
 use Illuminate\Http\Resources\Json\JsonResource;
@@ -16,30 +17,31 @@ class UserLoginResourse extends JsonResource
      */
     public function toArray($request)
     {
+        $normal_user=NormalUser::where('user_id',$this->id)->first();
         $token = auth('api')->login(User::find($this->id));
-        $devices[]=$this->devices;
+        $devices[]=$normal_user->devices;
         $devices[]=$request['device.id'];
-//        $this->update([
-//            'devices' => $devices,
-//            'last_login_at' => Carbon::now(),
-//            'last_ip' => $request->ip(),
-//        ]);
+        $normal_user->update([
+            'devices' => $devices,
+            'last_login_at' => Carbon::now(),
+            'last_ip' => $request->ip(),
+        ]);
         return [
             "user" => [
                 'id' => (int)$this->id,
-                'type' => $request['type'],
-                'name' => $this->name,
+                'type' => 'USER',
+                'name' => $normal_user->name,
                 'phone' => $this->phone ?? "",
                 'city' => [
-                    'id' => $this->city ? (int)$this->city->id : 0,
-                    'name' => $this->city ? $this->city->name : "",
+                    'id' => $normal_user->city ? (int)$normal_user->city->id : 0,
+                    'name' => $normal_user->city ? $this->city->normal_user->name : "",
                 ],
                 'district' => [
-                    'id' => $this->district ? (int)$this->district->id : 0,
-                    'name' => $this->district ? $this->district->name : "",
+                    'id' => $normal_user->district ? (int)$normal_user->district->id : 0,
+                    'name' => $normal_user->district ? $normal_user->district->name : "",
                 ],
-                'location'=>$this->location,
-                'image' => $this->image,
+                'location'=>$normal_user->location,
+                'image' => $normal_user->image,
             ],
             "access_token" => [
                 'token' => $token,
