991af12709d7f35a891cf4c9fb04e4894170c364
sa3d01 <sa3dsalem01@gmail.com>
2021-08-27 05:14:36 +0200

    new fucking edits

--- app/Http/Resources/OrderCollection.php
+++ app/Http/Resources/OrderCollection.php
@@ -2,7 +2,11 @@
 
 namespace App\Http\Resources;
 
+use App\Models\Delivery;
+use App\Models\DeliveryRequest;
+use App\Models\NormalUser;
 use App\Models\PromoCode;
+use App\Models\Provider;
 use App\Models\Setting;
 use Illuminate\Http\Resources\Json\ResourceCollection;
 use phpDocumentor\Reflection\Types\Object_;
@@ -13,24 +17,38 @@ class OrderCollection extends ResourceCollection
     {
         $data = [];
         foreach ($this as $obj) {
+            $normal_user=NormalUser::where('user_id',$obj->user_id)->first();
+            $provider=Provider::where('user_id',$obj->provider_id)->first();
             $arr['id'] = (int)$obj->id;
             $arr['status'] = $obj->status;
             $arr['user']['id'] = $obj->user_id;
-            $arr['user']['name'] = $obj->user->normal_user->name;
+            $arr['user']['name'] = $normal_user->name;
             $arr['provider']['id'] = $obj->provider_id;
-            $arr['provider']['name'] = $obj->provider->provider->name;
+            $arr['provider']['name'] = $provider->name;
             if ($obj->delivery_id==null){
                 $arr['delivery']=new Object_();
             }else{
+                $delivery_model=Delivery::where('user_id',$obj->delivery_id)->first();
                 $delivery['id']=$obj->delivery_id;
-                $delivery['name']=$obj->delivery->delivery->name;
+                $delivery['name']=$delivery_model->name;
                 $arr['delivery']=$delivery;
             }
+
             if ($obj['deliver_by']=='user') {
                 $delivery_price=0;
-            }else{
+            }elseif($obj['deliver_by']=='provider'){
                 $delivery_price=$obj->orderItems->first()->cartItem->product->user->provider->delivery_price;
+            }else{
+                $delivery_price=0;
+                if ($obj['delivery_id']!=null)
+                {
+                    $delivery_request=DeliveryRequest::where(['delivery_id'=>$obj['delivery_id'],'order_id'=>$obj->id,'status'=>'accepted'])->latest()->first();
+                    if ($delivery_request){
+                        $delivery_price=$delivery_request->delivery_price;
+                    }
+                }
             }
+
             $promo_code = PromoCode::where('code', $obj->promo_code)->first();
             if ($promo_code) {
                 $discount=$promo_code->discount_percent*($obj->price()+$delivery_price)/100;
