e09cbddd3930ff45e113272b10485e05f21cae65
sa3d01 <sa3dsalem01@gmail.com>
2021-08-31 21:30:27 +0200

    latest fucking edits

--- app/Http/Controllers/Api/Order/OrderStatusController.php
+++ app/Http/Controllers/Api/Order/OrderStatusController.php
@@ -118,7 +118,8 @@ class OrderStatusController extends MasterController
             return $this->sendError("ﻻ يمكنك اجراء هذه العملية");
         }
         $order->update([
-           'deliver_by'=>'user'
+           'deliver_by'=>'user',
+            'delivery_approved_expired'=>0
         ]);
         $normal_user=NormalUser::where('user_id',$order->user_id)->first();
         $provider_model=Provider::where('user_id',$order->provider_id)->first();
